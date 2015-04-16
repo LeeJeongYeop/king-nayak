0f1ce92f39315ad37f7a04b08313e19eae11218b
LEE <kingwjdduq@naver.com>
2015-04-16 20:57:01 +0900

    github test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '정여비' });
+  res.render('index', { title: 'Nayak' });
 });
 
 module.exports = router;
