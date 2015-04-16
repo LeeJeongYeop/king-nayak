64b73d810ab466b2fcefbe1a49bb2e98c12d452a
LEE <kingwjdduq@naver.com>
2015-04-16 23:58:33 +0900

    reset

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
-var express = require("express");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
-var nicknames = [];
-server.listen(80);
 
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/index.html');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-io.sockets.on('connection', function(socket){
-	socket.on('new user', function(nick, callback){
-		if(nicknames.indexOf(nick) != -1){
-			callback(false);
-		}else{
-			callback(true);
-			socket.nickname = nick;
-			nicknames.push(socket.nickname);
-			updateNicknames();
-		}
-	});
+// error handlers
 
-	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
-	}
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-	socket.on('send message', function(data){
-		io.sockets.emit("new message", {msg:data, nick:socket.nickname});
-	});
-
-	socket.on('disconnect', function(data){
-		if(!socket.nickname){ return; }
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
-	});
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+
+module.exports = app;
