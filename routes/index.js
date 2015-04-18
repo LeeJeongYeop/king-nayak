61d304da7ea41d4ccf89cbe07985a3aba08d6dc0
LeeJeongYeop <wjdduq914@gmail.com>
2015-04-18 20:49:19 +0900

    connect test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '강미는 바보야' });
+  res.render('index', { title: '강미는 바보야!!!' });
 });
 
 module.exports = router;
