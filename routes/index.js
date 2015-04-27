f862ad58a9533bcccf99c4888cfee4aae42a7460
LeeJeongYeop <wjdduq914@gmail.com>
2015-04-27 16:00:58 +0900

    eee

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { jeong: '잘생긴 정여비!!', kang: '바보는 깡미~~~' });
+  res.render('index', { jeong: '잘생긴 정여비!!', kang: '바보는 깡미~~~뿅뿅' });
 });
 
 module.exports = router;
